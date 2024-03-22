752a60612a2af79c87086334ba7217ed01dc5c5b
Jonathan Shimwell <mail@jshimwell.com>
2024-03-22 22:19:20 +0000

    extra tests to chec std dev as % of mean

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import NeSST as nst
 
 
@@ -34,3 +35,37 @@ def test_DTprimspecmoments_mean_with_tion():
     DTmean_hot, _ = nst.DTprimspecmoments(Tion=10.0e3)  # units eV
 
     assert DTmean_cold < DTmean_hot  # units eV
+
+def test_DTprimspecmoments_variance_with_tion():
+    # checks the relative magnitude of the var
+
+    _, DTvar_cold = nst.DTprimspecmoments(Tion=5.0e3)  # units eV
+    _, DTvar_hot = nst.DTprimspecmoments(Tion=10.0e3)  # units eV
+
+    assert DTvar_cold < DTvar_hot  # units eV**2
+
+def test_DDprimspecmoments_variance_with_tion():
+    # checks the relative magnitude of the var
+
+    _, DDvar_cold = nst.DDprimspecmoments(Tion=5.0e3)  # units eV
+    _, DDvar_hot = nst.DDprimspecmoments(Tion=10.0e3)  # units eV
+
+    assert DDvar_cold < DDvar_hot  # units eV**2
+
+def test_DDprimspecmoments_variance_relative_size():
+    # checks the relative magnitude of the var
+
+    DDmean, DDvar = nst.DDprimspecmoments(Tion=5.0e3)  # units eV
+
+    # convert from variance with units of eV**2 to standard deviation
+    # then check that the standard deviation is about 3% of the mean value
+    assert np.isclose((100/DDmean)*np.sqrt(DDvar), 3, atol=0.3)
+
+def test_DTprimspecmoments_variance_relative_size():
+    # checks the relative magnitude of the var
+
+    DTmean, DTvar = nst.DTprimspecmoments(Tion=5.0e3)  # units eV
+
+    # convert from variance with units of eV**2 to standard deviation
+    # then check that the standard deviation is about 3% of the mean value
+    assert np.isclose((100/DTmean)*np.sqrt(DTvar), 1, atol=0.3)
